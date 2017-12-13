c2a99078b9f3f07cbf8d6e34d08b28ce106ef2ae
Peter Tan <pxhtan@gmail.com>
2017-12-12 18:50:02 -0800

    reorder code for production

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import store from './reducers';
 
-import registerServiceWorker from './registerServiceWorker';
 import App from './App';
+import store from './reducers';
+import registerServiceWorker from './registerServiceWorker';
 
 render(
   <Provider store={store}>
